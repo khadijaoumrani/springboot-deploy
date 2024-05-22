f117881f67ff4b91f008f21490c820ae1ff2d44d
unknown <khadijaoumrani03@gmail.com>
2024-05-22 20:20:13 +0200

    first commit

---
+++ src/main/java/com/example/springbootdeploy/SpringbootDeployApplication.java
@@ -0,0 +1,19 @@
+package com.example.springbootdeploy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeployApplication.class, args);
+	}
+	@GetMapping("/")
+	public String home(){
+	return "welcomeh";
+	}
+
+}
